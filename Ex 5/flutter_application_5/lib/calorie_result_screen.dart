181e1377660aa663af7e328a9ecc3631e842c1bc
HitheshSoosaiManickam <hitheshs@karunya.edu.in>
2025-10-07 19:50:03 +0530

    init22

--- Ex 5/flutter_application_5/lib/calorie_result_screen.dart
+++ Ex 5/flutter_application_5/lib/calorie_result_screen.dart
@@ -9,7 +9,7 @@ class CalorieResultScreen extends StatelessWidget {
 
     final user = ModalRoute.of(context)!.settings.arguments as User;
     final totalcalorie = user.breakfast + user.lunch + user.dinner + user.snacks;
-    final rem_cal = user.dailycalorie - totalcalorie;
+    final remCal = user.dailycalorie - totalcalorie;
     return Scaffold(
       appBar: AppBar(title: Text("Calorie Details"), backgroundColor: Colors.green,),
       body : Container(
@@ -32,8 +32,8 @@ class CalorieResultScreen extends StatelessWidget {
               child: ListTile(
                 leading: Icon(Icons.food_bank),
                 title: Text('Consumed Calorie:  $totalcalorie'),
-                subtitle: Text('Remaining Calorie: $rem_cal'),
-                trailing: (rem_cal >= 0)? Text('You are within your daily goal', style: TextStyle(color: Colors.green, fontSize: 10),): Text('You exceeded your daily goal', style: TextStyle(color: Colors.red, fontSize: 10),
+                subtitle: Text('Remaining Calorie: $remCal'),
+                trailing: (remCal >= 0)? Text('You are within your daily goal', style: TextStyle(color: Colors.green, fontSize: 10),): Text('You exceeded your daily goal', style: TextStyle(color: Colors.red, fontSize: 10),
                 )
               ),
             ),
